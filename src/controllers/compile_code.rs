e2dc4c0bf6f224288b582daa7733609204c894f2
Lab Rat <dev.frolics@gmail.com>
2023-12-02 23:41:38 +0530

    fixup(quiz): trim ends for better output matching

--- src/controllers/compile_code.rs
+++ src/controllers/compile_code.rs
@@ -5,6 +5,7 @@ use zen::run_program;
 #[derive(Deserialize)]
 pub struct CodeCompileRequest {
     pub code: String,
+    pub input: String
 }
 #[derive(Serialize)]
 pub struct CodeOutputResponse {
@@ -32,7 +33,7 @@ pub async fn compile_code(
     extract::Json(user): extract::Json<CodeCompileRequest>,
 ) -> Json<CodeOutputResponse> {
     Json(CodeOutputResponse {
-        output: runnable_code(user.code, ""),
+        output: runnable_code(user.code, &user.input),
     })
 }
 
@@ -57,7 +58,7 @@ fn match_outputs(code: String, testcases: Vec<Testcase>) -> Vec<Result<bool, Str
         let code_instance = runnable_code(code.clone(), &testcase.input);
 
         match code_instance {
-            Ok(actual_output) => output_vec.push(Ok(actual_output == testcase.expected_output)),
+            Ok(actual_output) => output_vec.push(Ok(actual_output.trim_end() == testcase.expected_output)),
             Err(err) => output_vec.push(Err(err)),
         }
     }
